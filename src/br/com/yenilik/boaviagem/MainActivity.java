b81c2c6a23b6ea91cf16cb72b766c4e5100582ae
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2013-09-11 18:47:36 -0300

    Projeto BoaViagem - Android

---
+++ src/br/com/yenilik/boaviagem/MainActivity.java
@@ -0,0 +1,47 @@
+package br.com.yenilik.boaviagem;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	private EditText usuario;
+	private EditText senha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void entrarOnClick(View v) {
+    	String usuarioInformado = usuario.getText().toString();
+    	String senhaInformada = senha.getText().toString();
+    	
+    	if ("leitor".equals(usuarioInformado) && "123".equals(senhaInformada)) {
+			// vai para outra activity
+		} else {
+			String mensagemErro = getString(R.string.erro_autenticacao);
+			
+			Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT);
+			
+			toast.show();
+		}
+	}
+    
+}
